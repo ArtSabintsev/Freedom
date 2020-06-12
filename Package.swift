1b0596348c539cc741ea295d416e6d332dea9a2f
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2020-06-12 14:51:48 -0400

    Updated package.swift

--- Package.swift
+++ Package.swift
@@ -4,5 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Freedom",
     products: [.library(name: "Freedom", targets: ["Freedom"])],
-    targets: [.target(name: "Freedom", path: "Sources")]
+    targets: [.target(name: "Freedom", path: "Sources")],
+    platforms: [.iOS(.v9)],  
+    swiftLanguageVersions: [.v5]
 )
